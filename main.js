652a6359b48fa2055fbc7ee9409dd460d5d5f000
gabryel olavo <124881501+gabherhzdasveia@users.noreply.github.com>
2023-11-22 08:26:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+function camaro() {
+    document.getElementById("camaro_acelero").play();
+  }
+  
+  document.querySelector(".tecla_camaro").onclick = camaro;
+  
+  function carreragt() {
+    document.getElementById("carreragt_acelero").play();
+  }
+  
+  document.querySelector(".tecla_carreragt").onclick = carreragt;
+  
+  function ferrari() {
+    document.getElementById("ferrari_acelero").play();
+  }
+  
+  document.querySelector(".tecla_ferrari").onclick = ferrari;
+  
+  function golfao() {
+    document.getElementById("golfao_acelero").play();
+  }
+  
+  document.querySelector(".tecla_golfao").onclick = golfao;
+  
+  function aventador() {
+    document.getElementById("aventador_acelero").play();
+  }
+  
+  document.querySelector(".tecla_lambo_aventador").onclick = aventador;
+  
+  function huracan() {
+    document.getElementById("huracan_acelero").play();
+  }
+  
+  document.querySelector(".tecla_lambo_huracan").onclick = huracan;
+  
+  function mustangão() {
+    document.getElementById("mustang_acelero").play();
+  }
+  
+  document.querySelector(".tecla_mustangão").onclick = mustangão;
+  
+  function porsche911() {
+    document.getElementById("911_acelero").play();
+  }
+  
+  document.querySelector(".tecla_911").onclick = porsche911;
+  
+  function ram() {
+    document.getElementById("ram_acelero").play();
+  }
+  
+  document.querySelector(".tecla_ram").onclick = ram;
