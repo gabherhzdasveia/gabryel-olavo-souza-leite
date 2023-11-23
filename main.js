a5493113695f79ce5855780df492cbb9a24f47d8
gabryel olavo <124881501+gabherhzdasveia@users.noreply.github.com>
2023-11-23 10:05:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function manoelgomes() {
+    document.getElementById("manoelgomes").play();
+  }
+  
+  document.querySelector(".tecla_manoelgomes").onclick = camaro;
+  
+  function eversonzoio() {
+    document.getElementById("eversonolhos").play();
+  }
+  
+  document.querySelector(".tecla_eversonzoio").onclick = carreragt;
+  
+  function caminhao() {
+    document.getElementById("caminhao").play();
+  }
+  
+  document.querySelector(".tecla_caminhao").onclick = ferrari;
+  
+  function kungfu() {
+    document.getElementById("kungfu").play();
+  }
+  
+  document.querySelector(".tecla_kungfu").onclick = golfao;
+  
+  function turbo() {
+    document.getElementById("turbo").play();
+  }
+  
+  document.querySelector(".tecla_turbo").onclick = aventador;
+  
+  function freddyfazbear() {
+    document.getElementById("freddyfazbear").play();
+  }
+  
+  document.querySelector(".tecla_freddyfazbear").onclick = huracan;
