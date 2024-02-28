3d170ed39131996558b1683beb0a728ed40b008a
gabryel olavo <124881501+gabherhzdasveia@users.noreply.github.com>
2024-02-28 12:06:12 -0300

    Add files via upload

---
+++ variavel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variavel</title>
+</head>
+<body>
+    <php
+    $nome = "gabherhz";
+    const SOBRENOME = "olavo";
+    echo "meu nome eh $nome" . SOBRENOME ;
+    $peso = 100;
+    $idade = 16;
+    echo ""
+    
+   ?>
+</body>
+</html>
